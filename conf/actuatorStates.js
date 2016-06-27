b7fca6f7f1e986560a9922836cb685ff7a6c20e4
matiaso <matias.j.oria@gmail.com>
2016-06-27 16:15:48 -0300

    Invertido de señales que envía el rele

--- conf/actuatorStates.js
+++ conf/actuatorStates.js
@@ -2,12 +2,12 @@ var actuatorStates = {
 	1 : { 
         label : "Regando",
         pin: 4,
-        signal: 1
+        signal: 0
     },
 	2 : { 
         label : "No Regando",
         pin: 4,
-        signal: 0
+        signal: 1
     }
 };
 
